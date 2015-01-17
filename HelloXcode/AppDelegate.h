7523ab3242d986fa8ce4b1368dd41be3e1f3d275
ITCS2825 Kozlevcar <itcs2825_kozlevcar@lakeland-community-colleges-imac-9.local>
2015-01-16 19:02:48 -0500

    Initial Commit

---
+++ HelloXcode/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HelloXcode
+//
+//  Created by ITCS2825 Kozlevcar on 1/16/15.
+//  Copyright (c) 2015 lcc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
